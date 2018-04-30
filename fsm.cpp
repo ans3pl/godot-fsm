cc335e574bd9fd0ff1b0cfcdb719a667873846cd
Kamil <kamil2138@gmail.com>
2018-04-30 20:02:25 +0200

    license, refactoring

--- fsm.cpp
+++ fsm.cpp
@@ -7,47 +7,145 @@ FSM::FSM()
 
 }
 
+FSM::FSMProcessMode FSM::get_fsm_process_mode() const {
+    return fsm_process_mode;
+}
+void FSM::set_fsm_process_mode(FSMProcessMode p_mode) {
+
+    if (fsm_process_mode == p_mode)
+        return;
+
+    switch (fsm_process_mode) {
+        case FSM_PROCESS_PHYSICS:
+            if (is_physics_processing_internal()) {
+                set_physics_process_internal(false);
+                set_process_internal(true);
+            }
+            break;
+        case FSM_PROCESS_IDLE:
+            if (is_processing_internal()) {
+                set_process_internal(false);
+                set_physics_process_internal(true);
+            }
+            break;
+    }
+    fsm_process_mode = p_mode;
+}
+void FSM::_set_process(bool p_process) {
+    switch (fsm_process_mode) {
+        case FSM_PROCESS_PHYSICS: set_physics_process_internal(p_process && !paused); break;
+        case FSM_PROCESS_IDLE: set_process_internal(p_process && !paused); break;
+    }
+    processing = p_process;
+}
+
+void FSM::_create_blank_state_map(void){
+    for (int c = 0; c < get_child_count(); ++c){
+        Node* node = get_child(c);
+        if(node != NULL){
+            State* state = node->cast_to<State>(node);
+            if(state != NULL){
+                add_state(state);
+            }
+        }
+    }
+}
+
+void FSM::_connect_state_signals(void){
+    Array states = state_map.keys();
+    for(int i = 0; i < states.size(); ++i){
+        Node* node = states[i];
+        State* state = node->cast_to<State>(node);
+        state->connect("request_connection", this, "change_state");
+    }
+}
+
+void FSM::change_state(String connection){
+    transition=true;
+    previous_state = current_state;
+
+    Dictionary sm = state_map[current_state];
+    Node* n = sm[connection];
+    current_state = Object::cast_to<State>(n);
+}
+
+StringName FSM::get_state_name(){
+    return current_state->call("get_name");
+}
+
 void FSM::_notification(int p_what) {
     switch (p_what) {
         case NOTIFICATION_READY: {
-            set_process_internal(true);
+            _set_process(true);
 
             State* fs = Object::cast_to<State>(get_node(first_state));
             current_state=fs;
             transition=true;
 
             _create_blank_state_map();
-            connect_state_signals();
+            _connect_state_signals();
         } break;
 
         case NOTIFICATION_INTERNAL_PROCESS: {
-            Variant dt = get_process_delta_time();
+            if(fsm_process_mode!=FSM_PROCESS_IDLE)
+                return;
 
-            if(transition){
-                transition = false;
-                if(previous_state!=nullptr){
-                    if(previous_state->get_script_instance()){
-                        previous_state->get_script_instance()->call("_state_exit");
-                    }
-                }
-                previous_state=current_state;
+            _update();
+        } break;
 
-                if(current_state!=nullptr){
-                    if(current_state->get_script_instance()){
-                        current_state->get_script_instance()->call("_state_enter");
-                    }
-                }
-            }
-            if(current_state){
-                if(current_state->get_script_instance()){
-                    current_state->get_script_instance()->call("_state_update", dt);
-                }
-            }
+        case NOTIFICATION_INTERNAL_PHYSICS_PROCESS: {
+            if(fsm_process_mode!=FSM_PROCESS_PHYSICS)
+                return;
+
+            _update();
         } break;
     }
 }
 
-NodePath FSM::get_first_state(){
+void FSM::connect_state(Node* source, Node* destination, String connection){
+    Dictionary sm = state_map[source];
+    sm[connection] = Object::cast_to<Node>(destination);
+    state_map[source] = sm;
+}
+
+void FSM::add_state(State* s){
+    PoolStringArray connections = s->get_connections();
+    Node* n = Object::cast_to<Node>(s);
+
+    Dictionary con = state_map[n];
+    for (int i=0; i<connections.size(); ++i){
+        if(!con.has(connections[i])){
+            con[connections[i]]=NULL;
+        }
+    }
+    state_map[n] = con;
+}
+
+void FSM::_update() {
+    Variant dt = get_process_delta_time();
+    if(transition){
+        transition = false;
+        if(previous_state!=nullptr){
+            if(previous_state->get_script_instance()){
+                previous_state->get_script_instance()->call("_state_exit");
+            }
+        }
+        previous_state=current_state;
+
+        if(current_state!=nullptr){
+            if(current_state->get_script_instance()){
+                current_state->get_script_instance()->call("_state_enter");
+            }
+        }
+    }
+    if(current_state){
+        if(current_state->get_script_instance()){
+            current_state->get_script_instance()->call("_state_update", dt);
+        }
+    }
+}
+
+NodePath FSM::get_first_state() const{
     return first_state;
 }
 
@@ -64,5 +162,9 @@ void FSM::_bind_methods() {
     ClassDB::bind_method(D_METHOD("set_first_state", "state"), &FSM::set_first_state);
     ClassDB::bind_method(D_METHOD("get_first_state"), &FSM::get_first_state);
 
+    ClassDB::bind_method(D_METHOD("set_fsm_process_mode", "process_mode"), &FSM::set_fsm_process_mode);
+    ClassDB::bind_method(D_METHOD("get_fsm_process_mode"), &FSM::get_fsm_process_mode);
+
     ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "first state"), "set_first_state", "get_first_state");
+    ADD_PROPERTY(PropertyInfo(Variant::INT, "process_mode", PROPERTY_HINT_ENUM, "Fixed,Idle"), "set_fsm_process_mode", "get_fsm_process_mode");
 }
--- fsm.h
+++ fsm.h
@@ -11,75 +11,48 @@ class FSM : public Node {
     GDCLASS(FSM, Node);
 
     bool transition = false;
+    bool processing = false;
+    bool paused;
 
 protected:
     static void _bind_methods();
 
-
 public:
-    StringName get_state_name(){
-        return current_state->call("get_name");
-    }
+    enum FSMProcessMode {
+        FSM_PROCESS_PHYSICS,
+        FSM_PROCESS_IDLE
+    };
+
+    StringName get_state_name();
+    void add_state(State* s);
+
+    void change_state(String connection);
+    void connect_state(Node* source, Node* destination, String connection);
 
     void _notification(int p_what);
 
-    void _create_blank_state_map(void){
-        for (int c = 0; c < get_child_count(); ++c){
-            Node* node = get_child(c);
-            if(node != NULL){
-                State* state = node->cast_to<State>(node);
-                if(state != NULL){
-                    add_state(state);
-                }
-            }
-        }
-    }
-    void connect_state_signals(void){
-        Array states = state_map.keys();
-        for(int i = 0; i < states.size(); ++i){
-            Node* node = states[i];
-            State* state = node->cast_to<State>(node);
-            state->connect("request_connection", this, "change_state");
-        }
-    }
-
-    void change_state(String connection){
-        transition=true;
-        previous_state = current_state;
-
-        Dictionary sm = state_map[current_state];
-        Node* n = sm[connection];
-        current_state = Object::cast_to<State>(n);
-    }
-
-    void connect_state(Node* source, Node* destination, String connection){
-        Dictionary sm = state_map[source];
-        sm[connection] = Object::cast_to<Node>(destination);
-        state_map[source] = sm;
-    }
-
-    void add_state(State* s){
-        PoolStringArray connections = s->get_connections();
-        Node* n = Object::cast_to<Node>(s);
-
-        Dictionary con = state_map[n];
-        for (int i=0; i<connections.size(); ++i){
-            if(!con.has(connections[i])){
-                con[connections[i]]=NULL;
-            }
-        }
-        state_map[n] = con;
-    }
-
-    NodePath get_first_state();
+    NodePath get_first_state() const;
     void set_first_state(const NodePath &path);
+
+    FSMProcessMode get_fsm_process_mode() const;
+    void set_fsm_process_mode(FSMProcessMode p_mode);
 
     FSM();
 
+private:
     Dictionary state_map;
     State* previous_state = nullptr;
     State* current_state = nullptr;
     NodePath first_state;
+    FSMProcessMode fsm_process_mode;
+
+
+    void _set_process(bool p_process);
+    void _update();
+    void _create_blank_state_map(void);
+    void _connect_state_signals(void);
 };
+
+VARIANT_ENUM_CAST(FSM::FSMProcessMode);
 
 #endif // FSM_H
